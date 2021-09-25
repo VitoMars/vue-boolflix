a6cb38c1177822413024246f793eba318338c12a
Vito <vito95v@gmail.com>
2021-09-25 16:47:56 +0200

    Creazione struttura iniziale

---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="header">
+    <div class="logo">Boolflix</div>
+    <input type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 10vh;
+  width: 100%;
+  padding: 0px 50px;
+  background-color: black;
+}
+.logo {
+  font-size: 20px;
+  color: red;
+  text-transform: uppercase;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="main"></div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.main {
+  display: flex;
+  align-items: center;
+  height: 90vh;
+  width: 100%;
+  padding: 0px 50px;
+  background-color: grey;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
