fb8c19137924e1991e71758c1785742b0e4b0d7a
Vito <vito95v@gmail.com>
2021-09-28 17:02:23 +0200

    Fix card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,32 +1,27 @@
 <template>
-  <div class="carta">
-    <div class="img">
-      <img
-        class="poster"
-        :src="'https://image.tmdb.org/t/p/w342' + card.poster_path"
-        alt=""
-      />
-      <div class="retro">
-        <span><b>Titolo: </b> {{ card.title }}</span>
-        <span><b>Titolo originale: </b>{{ card.original_title }}</span>
-        <span><b>Voto: </b>{{ card.vote_average }}</span>
-        <div>
-          Lingua:
-          <img
-            class="flag"
-            :src="require(`../assets/flags/${card.original_language}.png`)"
-            alt=""
-          />
-        </div>
-      </div>
-    </div>
-    <!-- <img
-      class="flag"
-      v-if="flags.includes(item.original_language)"
-      :src="require(`../assets/flags/${item.original_language}.png`)"
+  <div class="Card">
+    <!-- Front Card -->
+    <img
+      class="poster"
+      :src="'https://image.tmdb.org/t/p/w342' + card.poster_path"
       alt=""
     />
-    <p v-else>{{ item.original_language }}</p> -->
+    <!-- Back Card -->
+    <div class="retro">
+      <span><b>Titolo: </b> {{ card.title }}</span>
+      <span><b>Titolo originale: </b>{{ card.original_title }}</span>
+      <span><b>Voto: </b>{{ card.vote_average }}</span>
+      <span>
+        Lingua:
+        <img
+          class="flag"
+          v-if="flags.includes(card.original_language)"
+          :src="require(`../assets/flags/${card.original_language}.png`)"
+          alt=""
+        />
+        <span v-else>{{ card.original_language.toUpperCase() }}</span>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -36,7 +31,7 @@ export default {
   data() {
     return {
       imgUrl: "https://image.tmdb.org/t/p/w342",
-      // flags: ["it", "en", "fr", "es", "de"],
+      flags: ["it", "en", "fr", "es", "de"],
     };
   },
   props: ["card"],
@@ -46,11 +41,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .flag {
-  height: 30px;
+  height: 20px;
   width: 30px;
 }
 
-.img {
+.Card {
+  margin: 10px;
   height: 350px;
   width: 230px;
   display: flex;
@@ -68,11 +64,11 @@ export default {
   padding: 0px 10px;
 }
 
-.img:hover img {
+.Card:hover .poster {
   display: none;
 }
 
-.img:hover .retro {
+.Card:hover .retro {
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -83,9 +79,5 @@ export default {
   height: 100%;
   width: 100%;
   padding: 0px 50px;
-}
-.cards {
-  display: flex;
-  flex-wrap: wrap;
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,21 +2,7 @@
   <div class="main">
     <h2>Films</h2>
     <ul class="cards">
-      <!-- <li v-for="(card, index) in movieCards" :key="index"> -->
       <Card v-for="(card, index) in movieCards" :key="index" :card="card" />
-      <!-- <div class="img" @mouseover="checkOverview()">
-          <img
-            :src="'https://image.tmdb.org/t/p/w342' + card.poster_path"
-            alt=""
-          />
-          <div class="retro">
-            <span><b>Titolo: </b> {{ card.title }}</span>
-            <span><b>Titolo originale: </b>{{ card.original_title }}</span>
-            <span><b>Voto: </b>{{ card.vote_average }}</span>
-            <span><b>Overview: </b>{{ lengthOverview }}</span>
-          </div>
-        </div> -->
-      <!-- </li> -->
     </ul>
   </div>
 </template>
@@ -29,58 +15,13 @@ export default {
     Card,
   },
   props: ["movieCards"],
-  data() {
-    return {
-      imgUrl: "https://image.tmdb.org/t/p/w342",
-      hover: false,
-      lengthOverview: "ciao",
-    };
-  },
-  methods: {
-    checkOverview() {
-      console.log(this.lengthOverview);
-      this.lengthOverview = this.card.overview;
-      if (this.lengthOverview.length > 50) {
-        this.lengthOverview.overview = this.lengthOverview.slice(0, 50) + "...";
-      }
-      return this.lengthOverview;
-    },
-  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h2 {
-  padding: 30px 0px;
-}
-.img {
-  height: 350px;
-  width: 230px;
-  display: flex;
-  background-size: cover;
-  background-color: black;
-}
-
-img {
-  height: 350px;
-  width: 230px;
-}
-
-.retro {
-  display: none;
-  padding: 0px 10px;
-}
-
-.img:hover img {
-  display: none;
-}
-
-.img:hover .retro {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  flex-wrap: wrap;
+  margin: 30px 10px 20px 10px;
 }
 
 .main {
